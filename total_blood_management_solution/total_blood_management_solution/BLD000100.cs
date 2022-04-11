ce877dc7430175822c602b40b44b3c3c934d30a4
Youngsuh Kim <dudtj0512@naver.com>
2022-04-12 01:14:06 +0900

    세부 페이지 구성

---
+++ total_blood_management_solution/total_blood_management_solution/BLD000100.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace total_blood_management_solution
+{
+    public partial class BLD000100 : Form
+    {
+        string str = "";
+        public BLD000100()
+        {
+            InitializeComponent();
+        }
+    }
+}
